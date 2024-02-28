6e3ddd6c32ae7050f64cf4d0612cc55a9d24798c
Francianeny <nirinasoabozy@yahoo.fr>
2024-02-28 10:02:51 +0000

    print alph 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-char n;
-for (n = 'a'; n <= 'z'; n++)
-_putchar(n);
-}
-if (i != 9)
-_putchar('\n');
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		char n;
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		if (i != 9)
+			_putchar('\n');
+	}
 }
