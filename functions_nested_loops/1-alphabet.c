7c55bcb1ffcb43eebb03d0a41d3bcb2d5fe48175
Francianeny <nirinasoabozy@yahoo.fr>
2024-02-28 08:16:25 +0000

    alphabet1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
-
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-_putchar('c');
+char n;
+for (n = 'a'; n <= 'z'; n++)
+_putchar(n);
 _putchar('\n');
-}
-int main(void)
-{
-print_alphabet();
 return (0);
 }
