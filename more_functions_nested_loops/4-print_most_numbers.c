e0d281ea28ab1e9259c541320b5977e5864e473d
Francianeny <nirinasoabozy@yahoo.fr>
2024-02-29 12:46:52 +0000

    print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check believe in number and sing
+ * (neither 2 nor 4).
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+  int n=48; 
+  for (n = 48 ; n < 58 ; n++);
+  if (n != 50 && n != 52)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
