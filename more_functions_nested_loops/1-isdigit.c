b7ce2e3ef912cccab7e91cc43d6daabb450d8723
Francianeny <nirinasoabozy@yahoo.fr>
2024-02-29 09:04:40 +0000

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+_isupper#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit -  checks for uppercase character.
+ * @c: character
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
