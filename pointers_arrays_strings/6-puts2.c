45e9368f68602075e913c1a00c7f0af3348db4a0
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-03 18:38:57 +0000

    puts half

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -14,9 +14,8 @@ void puts2(char *str)
 int i = 0;
 while (str[i] != '\0')
 {
-if (i % 2 == 0)
 _putchar(str[i]);
-i++;
+i += 2;
 }
 _putchar('\n');
 }
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* puts_half - half of a string
+* @str: half of a string
+*
+* Return: Always 0.
+*/
+void puts_half(char *str)
+{
+int len = _strlen(str);
+if (len % 2 == 0)
+len = len / 2;
+else
+len = (len + 1) / 2;
+while (str[len] != '\0')
+{
+_putchar(str[len]);
+len++;
+}
+_putchar('\n');
+}
