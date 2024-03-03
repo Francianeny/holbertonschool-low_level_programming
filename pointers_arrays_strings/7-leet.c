5f7587dc9fafffd965e910563ca8c6b4773caee0
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-03 22:34:41 +0000

    strncpy

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* leet - function that encode a string
+*@str:string that will be encoded
+*Return:returns encoded string
+*/
+
+int main(void)
+{
+char *p = leet("hello");
+}
+char *leet(const char *s)
+{
+char *result = malloc(strlen(s) + 1);
+return (result);
+}
