f9712fbb748eeb9d846d86db0e12ea11e31cae76
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-06 22:39:48 +0000

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,16 +12,15 @@ void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size; i += size)
-		sum1 += a[i];
+	for (i = 0; i < (size * size); i++)
 	{
-		for (i = size - 1; i < size * (size - 1); i += size - 1)
-			sum2 += a[i];
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
 		{
-			putchar(sum1 + '0');
-			putchar('\n');
-			putchar(sum2 + '0');
+			if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+				sum2 += *(a + i);
 		}
-		putchar('\n');
+		_putchar (sum2);
 	}
+	_putchar ('\n');
 }
