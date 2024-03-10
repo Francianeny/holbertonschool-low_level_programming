3f094f30da979fd1b7cd3e5b7aa2ea27d9fed6da
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-10 14:34:01 +0000

    print alphabet 10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
@@ -11,11 +11,10 @@ void print_alphabet_x10(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 'a'; b <= 'z'; b++)
+		for (b = 97; b <= 122; b++)
 		{
 			_putchar(b);
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
-	print_alphabet_x10();
 }
