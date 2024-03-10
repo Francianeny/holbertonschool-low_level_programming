1bea257c3058c5718affd4d1750c11af4ec56bb0
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-10 17:20:27 +0000

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
-* puts2 - prints every other character of a string, starting with the first character,
-*         
+* puts2 - prints every other character of a string
 * @str: string to print
 *
 * Return: void
