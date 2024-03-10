c7cfb66d3723d91a88e422669406ef78ca41061f
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-10 15:45:48 +0000

    last digit

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * _abs - Computes the absolute value
- * of an integer.
- *
  * @i: input number as an integer.
- *
  * Return: absolute value
  */
 int _abs(int i)
 {
-	if (i >= 0)
-	{
-		return (i);
-	}
-	else
-	{
-		return (i * -1);
-	}
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}
+
+int main(void)
+{
+int i = -1;
+int r = _abs(i);
+printf("%d\n", r);
+return (0);
 }
