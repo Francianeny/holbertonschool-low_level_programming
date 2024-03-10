7cdb7d73d9ed6b47dcfb079a3b4a19d6c336b4b5
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-10 17:22:34 +0000

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,23 @@
 *
 * Return: void
 */
-
 void puts_half(char *str)
 {
-int len = _strlen(str);
-int i;
-if (len % 2 == 0)
-i = len / 2;
-else
-i = (len - 1) / 2;
-{
-_putchar(str[i]);
-}
-_putchar('\n');
+	int count = 0, i;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
