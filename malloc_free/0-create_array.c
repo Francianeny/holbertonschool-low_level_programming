3ead9c308b2e7fcd9077c5b734c5e53d4711cf9f
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-11 10:15:00 +0000

    create ar

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,20 +9,25 @@
  *
  * Return: a pointer to the array
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *a;
 	unsigned int i;
 
 	if (size == 0)
+	{
 		return (NULL);
+	}
 	a = malloc(sizeof(a) * size);
 
-	if (size == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++);
+	if (a == 0)
 	{
-		a(i) = c;
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
 	}
 	return (a);
 }
