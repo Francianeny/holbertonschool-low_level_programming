8ef8e22f4ec1e2783c3f3390bdad741a112e5037
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-13 17:18:26 +0000

    mh

---
+++ more_malloc_free/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+#endif /* MAIN_H */
