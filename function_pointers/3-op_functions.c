a636a3c441d0a6525d4eab65032a5a8f158ea135
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-19 12:11:13 +0000

    op function

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ *  op_ -operator
+ * @a: num1
+ * @b: num2
+ * Return: function result
+ */
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+int op_mod(int a, int b)
+{
+	return(a % b);
+}
