558b64dc0d87ae19ba6e330db9978ef6f3a55f5a
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-19 17:59:43 +0000

    0 sum

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,12 +9,19 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	va_start(args,n);
-
 	unsigned int i;
-	for(unsigned int i = 0; i < n; i++)
-		if(n ==0);
+	int sum = 0;
+	if(n == 0);
 	{
 		return(0);
+	}
+
+	va_start(args, n);
+	for(unsigned int i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+	{
+		return(sum);
 	}
 }
