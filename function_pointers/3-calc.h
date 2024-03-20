44766f23e04fcc5014bf705d2e1bb5e6496b2b37
Francianeny <nirinasoabozy@yahoo.fr>
2024-03-20 13:09:31 +0000

    op functions

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef CALC_H
+#define CALC_H
 /**
  * struct op - Struct op
  *
@@ -10,8 +12,6 @@ typedef struct op
     int (*f)(int a, int b);
 } op_t;
 
-#ifndef 3-calc.h
-#define 3-calc.h
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,28 +2,72 @@
 #include "function_pointers.h"
 #include "3-calc.h"
 /**
- *  op_ -operator
+ * get_op_func - function that selects the correct function
+ * @s: is the operator passed as argument to the program
+ * 
+ * Return: function result
+ */
+op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+/**
+ * op_add - addition operator
  * @a: num1
  * @b: num2
  * Return: function result
  */
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
+
+/**
+ * op_sub - subtraction operator
+ * @a: num1
+ * @b: num2
+ * Return: function result
+ */
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
+
+/**
+ * op_mul - multiplication operator
+ * @a: num1
+ * @b: num2
+ * Return: function result
+ */
 int op_mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
+
+/**
+ * op_div - division operator
+ * @a: num1
+ * @b: num2
+ * Return: function result
+ */
 int op_div(int a, int b)
 {
-	return(a / b);
+	return (a / b);
 }
+
+/**
+ * op_mod - modulo operator
+ * @a: num1
+ * @b: num2
+ * Return: function result
+ */
 int op_mod(int a, int b)
 {
-	return(a % b);
+	return (a % b);
 }
