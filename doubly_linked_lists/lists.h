27d49f877471f0b215d23c0cbc91df08d417a69c
Francianeny <nirinasoabozy@yahoo.fr>
2024-04-02 09:38:32 +0000

    lists h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 typedef struct dlistint_s
 {
     int n;
@@ -15,4 +19,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+
 #endif /* LISTS_H */
