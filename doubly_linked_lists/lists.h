1528cd6c5edb81609faf6afa67daad7c23c978ad
Francianeny <nirinasoabozy@yahoo.fr>
2024-04-02 07:41:28 +0000

    lists h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -9,4 +9,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 #endif /* LISTS_H */
