ca4c731fe112dc568d4434edb08d9bf5c7551cc8
Francianeny <nirinasoabozy@yahoo.fr>
2024-04-04 20:57:58 +0000

    4-clear_bit.c

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -16,11 +16,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > 63)
 		return (-1);
-
-	m = 1 << index;
+		m = 1 << index;
 
 	if (*n & m)
 		*n ^= m;
-}
-
 	return (1);
+}
