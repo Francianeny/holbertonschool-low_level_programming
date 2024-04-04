cdab9baea922e354fac9750e7efc2978570d42c9
Francianeny <nirinasoabozy@yahoo.fr>
2024-04-04 21:13:25 +0000

    1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -15,11 +15,12 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+
+		putchar((n & 1) + '0');
 	}
 	else
 	{
-		_putchar('0');
+		putchar('0');
 	}
-	return (void);
+	return;
 }
